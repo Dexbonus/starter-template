30a5a7fbf8469a30e0a39b7137b70b37d58d3b90
rhetoric <miketanner3128@gmail.com>
2017-06-22 02:10:41 -0400

    initial commit

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -21,18 +21,19 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    vendor: ['axios']
     /*
     ** Run ESLINT on save
     */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+    // extend (config, ctx) {
+    //   if (ctx.isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
   }
 }
